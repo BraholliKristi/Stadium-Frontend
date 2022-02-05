a9d78804914af009fdb5136d925cdbf9d3d30bfb
Kristi Braholli <62339849+BraholliKristi@users.noreply.github.com>
2022-02-05 02:07:52 +0100

    Added Rooms design and events

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import Breadcrums from "./BreadCrums";
 import Main from "./components/Main";
 import {fetchEvents} from "../lib/api";
 import CarouselSlide from "./components/carouselSlider";
-
+import bck1 from "../public/img/bck1.jpg";
 export default function home(props) {
 console.log(props.data.length)
   return (
       <>
         <NavBar/>
-          <CarouselSlide/>
+          <CarouselSlide image={bck1}/>
           <Breadcrums Title={"Leatest Events"}/>
           <Main data={props.data}/>
         <Footer/>
