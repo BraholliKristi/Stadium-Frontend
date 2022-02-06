0d80c96cf1dd9f42981d389b8a308758bdac078f
Kristi Braholli <62339849+BraholliKristi@users.noreply.github.com>
2022-02-06 21:53:34 +0100

    Finished without PayPal Payment.

--- pages/components/events/EventsCards.js
+++ pages/components/events/EventsCards.js
@@ -3,12 +3,12 @@ import img1 from "../../../public/img/bck1.jpg";
 import Link from "next/link";
 export default function EventCards(props){
     return(
-        <div className={"col-span-4 p-10"}>
+        <div className={" col-span-4 p-10"}>
             <Link href={"events/"+props.data.id} >
                <a>
-                       <div className="max-w-sm rounded h-full overflow-hidden shadow-lg">
+                       <div className="relative max-w-sm rounded h-full overflow-hidden shadow-lg">
                            <Image className="w-full" width={380} height={200} src={"/img/"+props.data.image_path} alt="Sunset in the mountains"/>
-                           <div className="px-6 py-4">
+                           <div className="px-6 py-4 ">
                                <div className="font-bold text-xl mb-2">{props.data.name}</div>
                                <div className="text-xl">
                                    <p className="text-gray-700 text-strong">
@@ -17,11 +17,16 @@ export default function EventCards(props){
                                    <p className="text-gray-700 text-strong">
                                        <b> Time:</b> {props.data.time}
                                    </p>
+
                                </div>
+                               <button className={"absolute p-2 m-0.5 border border-red-700 rounded-2xl right-0 bottom-0 hover:bg-red-600 hover:text-white"}>
+                                   Check Out
+                               </button>
                                <p className="text-gray-700 text-base">
                                    {(props.data.description).substring(0,56)+"..."}
                                </p>
                            </div>
+
                        </div>
                </a>
             </Link>
