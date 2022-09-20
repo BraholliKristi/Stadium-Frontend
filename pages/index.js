05978bb0b7c2d0b986965495fe2004160231c3e2
Kristi Braholli <krist@oxa.al>
2022-09-20 23:35:26 +0200

    Test

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,17 @@ import CarouselSlide from "./components/carouselSlider";
 import bck1 from "../public/img/bck1.jpg";
 import VideoHeader from "./components/VideoHeader";
 export default function home(props) {
-console.log(props.data.length)
+/*console.log(props.data.length)*/
     let Bcks=['bck1.jpg','bck2.jpg','bck3.jpg'];
   return (
       <>
         <NavBar/>
           <VideoHeader url={"/video/bckIntro.mp4"} title={"WELCOME TO AIR ALBANIA"}/>
           <Breadcrums Title={"Latest Events"}/>
-          <Main data={props.data} />
+
         <Footer/>
       </>
   )
 }
 
-export async function getServerSideProps() {
-    const data=await fetchEvents();
 
-    return {
-        props: {
-            data,
-
-        }
-    };
-}
